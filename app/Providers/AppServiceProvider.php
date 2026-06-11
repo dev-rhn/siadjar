3d6e2a82fd2247fbdf64e6445f408fc2ca145e47
Raihan Akbar <raihan.akbar0354@gmail.com>
2026-06-11 11:49:58 +0700

    FIX: Add Classes URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
